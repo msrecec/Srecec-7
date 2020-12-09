75d5dcf2e5cbc2333f38b3f7c03952c8475e8816
Mislav Srecec <mislav.srecec3@gmail.com>
2020-12-09 18:39:25 +0100

    Starting Setup

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"main.java.hr.java.covidportal.iznimke"},{"l":"main.java.hr.java.covidportal.main"},{"l":"main.java.hr.java.covidportal.model"}];updateSearchResults();
